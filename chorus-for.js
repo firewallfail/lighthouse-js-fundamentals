be573fc8c49a535a0bca6265c8251381908f4929
firewallfail <firewall.fail@hotmail.com>
2021-11-12 21:39:49 -0500

    Use a for loop to repeat a chorus line

---
+++ chorus-for.js
@@ -0,0 +1,6 @@
+//does the same thing as the while from chorus-while.js
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
