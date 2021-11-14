628987bd169828788f307b71d99d2b88ae4ea8e4
firewallfail <firewall.fail@hotmail.com>
2021-11-14 14:45:12 -0500

    Difference between return and console.log

--- hello.js
+++ hello.js
@@ -1,9 +1,11 @@
+//Prints "Hello, world" when the function is called
 // const sayHello = function () {
 //   console.log("Hello, world");
 // }
 
 // sayHello();
 
+//Prints "Hello, (name)" when function is called with an arguement
 const sayHello = function (name) {
   console.log("Hello, " + name);
 }
@@ -11,3 +13,15 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+//prints "Hello, " + name to console without returning anything from the function
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+//doesn't print anything but returns "Hello, " + name to be passed into something else ( variable here )
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
