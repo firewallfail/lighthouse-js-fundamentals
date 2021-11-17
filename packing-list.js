a4c80bc0fcd99872dbef70adbd3abc9b8040e48b
firewallfail <firewall.fail@hotmail.com>
2021-11-17 11:27:05 -0500

    printing 1 item from an array at a time using a while loop

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,15 @@
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+// const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+// console.log("Kitchen stuff to pack:");
+// //loop through packing list, display 1 item at a time
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
+//same as the previous for loop but using while
 console.log("Kitchen stuff to pack:");
-//loop through packing list, display 1 item at a time
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
