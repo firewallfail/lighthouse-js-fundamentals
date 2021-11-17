26d13ae345ef7d1a33ee9d9132541faba7e25900
firewallfail <firewall.fail@hotmail.com>
2021-11-17 11:25:53 -0500

    printing 1 item from an array at a time

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+//loop through packing list, display 1 item at a time
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
